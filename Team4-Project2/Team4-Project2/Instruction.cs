3e413d6259c58e954d8f2c8b0de9c58701e2f124
William <Simmonswa@etsu.edu>
2022-03-07 23:22:38 -0500

    instruction class

---
+++ Team4-Project2/Team4-Project2/Instruction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team4_Project2
+{
+    class Instruction
+    {
+        string name = String.Empty;
+        int fetch, decode, execute, store = 0;
+
+        public Instruction(string name, int Fetch, int decode, int execute, int store)
+        {
+            this.name = name; 
+        }
+    }
+}
